24a3a0217f3a8908a3715c57184ee83b3e8fde94
sebazdev <szuviria@gmail.com>
2023-03-02 21:04:06 -0300

    Clase de componentes 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+// import Counter from './components/Counter/Counter';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        {/* <Counter title={"manzanas"} initial={1} stock={5}/>
+        <Counter title={"naranjas"} initial={1} stock={6}/> */}
+        <ItemListContainer greeting={"Bienvenidos a mi Ecommerce"}/>
     </div>
   );
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+const Button = (props) => {
+    return <button 
+                onClick={props.handleClick}
+                style={{
+                    color: 'white',
+                    backgroundColor: 'red'
+                }}
+            >
+                    {props.label}
+            </button>
+}
+
+export default Button
